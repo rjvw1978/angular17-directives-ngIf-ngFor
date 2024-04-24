5bd65dd0efe937a563895253694e95563701c75d
Rojas Córsico Ivana Soledad <irojas@gmail.com>
2024-04-24 20:44:50 -0300

    ngIf, ngFor example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, NgFor,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular17-directives-Structure';
+  imagenList: { url: string, name: string }[] = [
+    { url: "./assets/child.jpg", name:"Niña jugando" },
+    { url: "./assets/ai-generated.jpg", name:"Miradas"},
+    { url: "./assets/couple.webp", name:"Pareja de Ancianos"},
+    { url: "./assets/crayons.jpg", name:"Colores"},
+  ];
+
 }
