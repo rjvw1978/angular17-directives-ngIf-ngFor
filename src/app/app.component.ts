e34ae97353b52b9de386b4cf796d8f88bf3923e3
Rojas Córsico Ivana Soledad <irojas@gmail.com>
2024-04-24 21:23:56 -0300

    updated to add category

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
-import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { CommonModule, NgFor, NgIf, NgSwitch, NgSwitchCase } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NgFor,NgIf],
+  imports: [CommonModule, NgFor,NgIf, NgSwitch, NgSwitchCase],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  imagenList: { url: string, name: string }[] = [
-    { url: "./assets/child.jpg", name:"Niña jugando" },
-    { url: "./assets/ai-generated.jpg", name:"Miradas"},
-    { url: "./assets/couple.webp", name:"Pareja de Ancianos"},
-    { url: "./assets/crayons.jpg", name:"Colores"},
+  imagenList: { url: string, name: string , categoria:number}[] = [
+    { url: "./assets/child.jpg", name:"Niña jugando", categoria:1 },
+    { url: "./assets/ai-generated.jpg", name:"Miradas" , categoria:4},
+    { url: "./assets/couple.webp", name:"Pareja de Ancianos", categoria:2},
+    { url: "./assets/crayons.jpg", name:"Colores", categoria:3},
   ];
 
 }
